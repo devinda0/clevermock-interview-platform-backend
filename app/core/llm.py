de62aca115b0f25f8742416f4bc26ef19739aa65
Devinda Dilshan <devindadilshan0@gmail.com>
2025-12-14 20:24:21 +0530

    refactor: switch LLM implementation from OpenRouter/OpenAI to Google Generative AI.

--- app/core/llm.py
+++ app/core/llm.py
@@ -1,13 +1,9 @@
-from langchain_openai import ChatOpenAI
-# from google.generativeai import genai
+from langchain_google_genai import ChatGoogleGenerativeAI
 from app.core.config import settings
 
 def get_llm():
-    return ChatOpenAI(
-        # model="kwaipilot/kat-coder-pro:free",
-        model="amazon/nova-2-lite-v1:free",
-        openai_api_key=settings.OPENROUTER_API_KEY,
-        openai_api_base="https://openrouter.ai/api/v1",
+    return ChatGoogleGenerativeAI(
+        model="gemini-2.0-flash-lite",
+        google_api_key=settings.GOOGLE_API_KEY,
         temperature=0.7
     )
-    # return genai.Client(api_key=settings.GOOGLE_API_KEY)
